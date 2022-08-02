b081f85e28ec5d2d5310c85a5f6d6599fde8c033
Francisco Andrade <franciscoezequielandrade@gmail.com>
2022-08-02 11:01:40 -0300

    agregamos el state tarea, arreglo tarea y enviamos por prop de formulario a listatareas

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Button } from 'react-bootstrap';
 import ListaTareas from './ListaTareas';
+import './formulario.css';
 
 const Formulario = () => {
+    // Aqui va la logica
+    // crear un state
+    const [arregloTareas, setArregloTareas] = useState([]);
+    const [tarea, setTarea] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setArregloTareas([...arregloTareas,tarea]);
+        setTarea('');
+    }
+
+    // Aqui va el maquetado y un poco de logica
     return (
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3 d-flex">
-                    <Form.Control type="text" placeholder="Enter email" />
+                    <Form.Control 
+                        type="text" 
+                        placeholder="Enter email" 
+                        onChange={(e)=>setTarea(e.target.value.trim())} 
+                        value={tarea}
+                    />
                     <Button variant="primary" type="submit">Enviar</Button>
                 </Form.Group>
             </Form>
             {/* Aqui invoco componente */}
-            <ListaTareas></ListaTareas>
+            <ListaTareas arregloTareas={arregloTareas}></ListaTareas>
         </div>
     );
 };
